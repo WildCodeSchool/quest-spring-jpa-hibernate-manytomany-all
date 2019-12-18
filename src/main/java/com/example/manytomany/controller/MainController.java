d8036f4fc8bfe58250966938b5fd1905efa68978
bastienwcs <email>
2019-12-18 14:29:50 +0100

    fix

--- src/main/java/com/example/manytomany/controller/MainController.java
+++ src/main/java/com/example/manytomany/controller/MainController.java
@@ -7,10 +7,6 @@ import com.example.manytomany.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
@@ -22,8 +18,7 @@ public class MainController {
     private StudentRepository studentRepository;
 
     @GetMapping("/init")
-    @ResponseBody
-    public List<Student> init() {
+    public String init() {
 
         schoolRepository.deleteAll();
         studentRepository.deleteAll();
@@ -32,16 +27,8 @@ public class MainController {
         School bordeaux = schoolRepository.save(new School("Bordeaux"));
         School lyon = schoolRepository.save(new School("Lyon"));
 
-        List<Student> students = new ArrayList<>();
         Student charlie = studentRepository.save(new Student("Charlie"));
-        Student louison = studentRepository.save(new Student("Louison"));
-        Student camille = studentRepository.save(new Student("Camille"));
-        Student andrea = studentRepository.save(new Student("Andréa"));
-        students.add(charlie);
-        students.add(louison);
-        students.add(camille);
-        students.add(andrea);
 
-        return students;
+        return "data initialized";
     }
 }
